ded499bc2386256673723dce952c077c0c35a4f6
Carlos Meza <carlos@digitalr00ts.com>
2019-11-14 09:30:31 -0800

    improved errors and exception handling

--- src/boto_remora/aws/helper.py
+++ src/boto_remora/aws/helper.py
@@ -1,9 +1,11 @@
 import logging
+
 from functools import partial
 from typing import FrozenSet
 
 import boto3
 import botocore
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,13 +18,23 @@ def is_region_accessible(region, session):
     client = session.client("sts", region_name=region)
     try:
         client.get_caller_identity()
-    except botocore.exceptions.ClientError:
-        _LOGGER.debug("Unable to access region %s.", region)
+    except botocore.exceptions.ClientError as err:
+        _LOGGER.debug(
+            "%snable to access region %s.",
+            f"Profile {session.profile_name} is u" if session.profile_name else "U",
+            region,
+        )
+        # https://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html#ErrorCodeList
+        if err.response["Error"]["Code"] == "ExpiredToken":
+            raise err from None
+        _LOGGER.debug(err)
         return False
     return True
 
 
-def get_accessible_regions(service_name: str, session: boto3.session.Session) -> FrozenSet[str]:
+def get_accessible_regions(
+    service_name: str, session: boto3.session.Session
+) -> FrozenSet[str]:
     """ Returns enabled regions from a given session. """
     # TODO: Add partition
     regions = session.get_available_regions(service_name)
@@ -30,8 +42,6 @@ def get_accessible_regions(service_name: str, session: boto3.session.Session) ->
     available_regions = frozenset(filter(is_region_accessible_, regions))
 
     if not available_regions:
-        _LOGGER.error(
-            "Access to all regions failed. Credentials may be invalid or there is a network issue."
-        )
+        _LOGGER.error("Access to all regions failed. There may be a network issue.")
 
     return available_regions
